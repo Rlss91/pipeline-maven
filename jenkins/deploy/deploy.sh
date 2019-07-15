cc172f5f795216e62ab4ecb405c3d3eea23ef610
Your Name <you@example.com>
2019-07-15 16:21:58 -0400

    update ssh key path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,10 +4,10 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i ../centos7/remote-key /tmp/.auth jenkins@10.0.0.56:/tmp/.auth
+scp -i /opt/prod /tmp/.auth jenkins@10.0.0.56:/tmp/.auth
 
-scp -i ../centos7/remote-key jenkins/deploy/publish.sh jenkins@10.0.0.56:/tmp/publish
+scp -i /opt/prod jenkins/deploy/publish.sh jenkins@10.0.0.56:/tmp/publish
 
-ssh -i ../centos7/remote-key jenkins@10.0.0.56 "chmod +x /tmp/publish"
+ssh -i /opt/prod jenkins@10.0.0.56 "chmod +x /tmp/publish"
 
-ssh -i ../centos7/remote-key jenkins@10.0.0.56 "/tmp/publish"
+ssh -i /opt/prod jenkins@10.0.0.56 "/tmp/publish"
